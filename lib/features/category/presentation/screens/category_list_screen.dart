7c53a6feb596ac741df4425a8037f56ff36f4aee
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-04-22 10:35:21 +0600

    Added category_list_screen with pagination

--- lib/features/category/presentation/screens/category_list_screen.dart
+++ lib/features/category/presentation/screens/category_list_screen.dart
@@ -1,7 +1,8 @@
-import "package:crafty_bay/features/shared/presentation/providers/category_list_provider.dart";
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
 
+import "../../../auth/presentation/widgets/center_progress_indicator.dart";
+import "../../../shared/presentation/providers/category_list_provider.dart";
 import "../../../shared/presentation/providers/main_nav_provider.dart";
 import "../../../shared/presentation/widgets/category_card.dart";
 
@@ -15,6 +16,30 @@ class CategoryListScreen extends StatefulWidget{
 }
 
 class _CategoryListScreenState extends State<CategoryListScreen> {
+
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll(){
+    final provider = context.read<CategoryListProvider>();
+
+    if (_scrollController.position.extentBefore < 300 && !provider.isLoading && provider.hasMore){
+      provider.getCategories();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
 
@@ -39,23 +64,34 @@ class _CategoryListScreenState extends State<CategoryListScreen> {
           padding: const EdgeInsets.all(8.0),
           child: Consumer<CategoryListProvider>(
             builder: (context, provider, child) {
-              return GridView.builder(
 
-                  itemCount: provider.categoryList.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      mainAxisSpacing: 8,
-                      childAspectRatio: 1,
+              if (provider.initialDataInProgress) return const CenterProgressIndicator();
+
+              return Column(
+                children: [
+                  Expanded(
+                    child: GridView.builder(
+
+                        controller: _scrollController,
+                        itemCount: provider.categoryList.length + 1,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            mainAxisSpacing: 8,
+                            childAspectRatio: 1,
+                        ),
+                        itemBuilder: (BuildContext context, int index){
+
+                          return FittedBox(
+                              child: CategoryCard(
+                                categoryModel: provider.categoryList[index],
+                              )
+                          );
+
+                        },
+                    ),
                   ),
-                  itemBuilder: (BuildContext context, int index){
-
-                    return FittedBox(
-                        child: CategoryCard(
-                          categoryModel: provider.categoryList[index],
-                        )
-                    );
-
-                  },
+                  if(provider.moreDataInProgress) const CenterProgressIndicator()
+                ],
               );
             }
           ),
@@ -66,8 +102,4 @@ class _CategoryListScreenState extends State<CategoryListScreen> {
 
   }
 
-
-
-
 }
-
